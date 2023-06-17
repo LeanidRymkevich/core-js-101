ca06ecbc66a96bf42d6a7f0dd9a99c91f579b5f6
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-06-18 00:44:36 +0300

    feat: add solutions to the first three functions-n-closures-tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 
@@ -62,8 +62,15 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...arg) {
+  if (!arg.length) return null;
+  return (x) => {
+    let result = 0;
+    for (let i = 0, j = arg.length - 1; i < arg.length; i += 1, j -= 1) {
+      result += arg[i] * x ** j;
+    }
+    return result;
+  };
 }
 
 
